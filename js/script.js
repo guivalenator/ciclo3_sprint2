aced9279f20f0fd5180b6de1ae3b08a195f2d5fa
guivalenator <guivalenator@gmail.com>
2021-09-23 22:08:51 -0500

    v1

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var aspectoBtn = document.getElementById("aspecto");
-var mainElement = document.getElementById("contenedor2");
-
-aspectoBtn.addEventListener("click", function(){
-mainElement.classList.toggle("dark");
-});
-//document.getElementById("aspecto").addEventListener("click",console.log("hola"));
